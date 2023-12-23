b8673280804a55195f6cb3f68b2e5c7b46e1f441
NinjaTyper <peter.lethien@gmail.com>
2023-12-22 16:19:45 -0800

    added templates

--- Website1/login/models.py
+++ Website1/login/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from unicodedata import name
 
 # Create your models here.
+class AccountLevel(models.Model):
+    chatspeed = models.IntegerField(default=5)
+
+
+class User(models.Model):
+    username = models.CharField(max_length = 100)
+    password = models.CharField(max_length = 100)
+    accountLvl = models.ForeignKey(AccountLevel, on_delete=models.PROTECT, default=1)
+
+    def __str__(self):
+        return "Username = " + self.username + ", Password = " + self.password + ", Account Level = " + str(self.accountLvl)
+    
